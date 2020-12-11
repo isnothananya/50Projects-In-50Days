e3c6f5779f5e2c419b6bea95997834f00f7b22cf
Thananya Rungjanyarak <isnothananya@users.noreply.github.com>
2020-12-11 23:44:29 +0700

    HTML

---
+++ 06-Scroll-Animation/app.js
@@ -0,0 +1,11 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes)
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+        
+    })
+}  
